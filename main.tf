132e931e2d5aa27cf16703b69a8788a85e8add58
Oleksii Smiichuk <smiychuk@gmail.com>
2023-06-15 19:37:31 +0000

    Added kms

--- main.tf
+++ main.tf
@@ -28,3 +28,24 @@ module "tls_private_key" {
   source    = "github.com/oleksiihead/tf-hashicorp-tls-keys"
   algorithm = "RSA"
 }
+
+module "gke-workload-identity" {
+  source              = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
+  use_existing_k8s_sa = true
+  name                = "kustomize-controller"
+  namespace           = "flux-system"
+  project_id          = var.GOOGLE_PROJECT
+  cluster_name        = "main"
+  location            = var.GOOGLE_REGION
+  annotate_k8s_sa     = true
+  roles               = ["roles/cloudkms.cryptoKeyEncrypterDecrypter"]
+}
+
+module "kms" {
+  source          = "github.com/oleksiihead/terraform-google-kms"
+  project_id      = var.GOOGLE_PROJECT
+  keyring         = "sops-flux"
+  location        = "global"
+  keys            = ["sops-key-flux"]
+  prevent_destroy = false
+}
